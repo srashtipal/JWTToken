541ff6778d7b7ba10c1e945c00ba3802033f8568
srashtipal <srashtipal64@gmail.com>
2024-12-24 21:09:23 +0530

    Employee controller for checking Token Data type ,Details ,And User

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using JWTToken.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with Jwt";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with Jwt";
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("AddUser")]
+        public string AddUser(Users user)
+        {
+            return "User added with username"+user.Username;
+        }
+    }
+}
